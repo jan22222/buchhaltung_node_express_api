ef23aa97e1c6472b200163d1b08b5933ba5438de
unknown <jan.weitzel@gmail.com>
2023-02-27 12:59:14 +0100

    taskmodel edited

--- app/models/table.model.js
+++ app/models/table.model.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const {CellSchema} = require("./cell.model.js")
  const TableSchema = mongoose.Schema({
 	 title: String,
-	 cells: [CellSchema]
+	 cells: [
+		{type: mongoose.Schema.Types.ObjectId,
+      	ref:"Cell"}
+	 ]
 });
 
 const TableModel = mongoose.model('TableModel', TableSchema)
